5f2b9d514c3cf5afe2318539cea5d3e589267d7e
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-07-30 17:07:47 -0300

    second commit

---
+++ Profile/src/content_option.js
@@ -0,0 +1,133 @@
+const logotext = "JORGE";
+const meta = {
+    title: "Jorge Lozano",
+    description: "I’m Jorge Lozano Full stack devloper,currently working in Argentina",
+};
+
+const introdata = {
+    title: "I’m Jorge Lozano",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I am a passionate full stack developer and QA tester, dedicated to building web applications that deliver exceptional user experiences. My strong foundation in the backend is based on the use of technologies such as Node.js and Express, allowing me to efficiently manage data through PostgresSQL. In addition, I implement a clear modularization of the code, which facilitates teamwork. At the same time, I'm continually honing my frontend skills, developing interactive interfaces with React and Redux, and styling with CSS to generate visually appealing and responsive web designs.",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a passionate full stack developer and QA tester, dedicated to building web applications that deliver exceptional user experiences. My solid foundation in the backend is based on the use of technologies such as Node.js and Express, allowing me to efficiently manage data through PostgresSQL. In addition, I implement a clear modularization of the code, which facilitates teamwork. . What really sets me apart is my unwavering commitment to continuous learning and always being at the forefront of web development. I am enthusiastic about new technologies and trends, and I strive to offer cutting-edge solutions for today's needs. My passion lies in tackling challenges and seeking technological solutions to improve people's well-being, leaving my mark on each project. I had the privilege of contributing to the development of <Aprende Conmigo>, where I played a vital role as developer and team leader. Using Scrum and Agile methodologies, we managed to achieve our objectives efficiently and on time, creating an innovative product that satisfies a need in the area of ​​education. I invite you to explore my portfolio and discover how my passion for learning and my dedication to achieving impactful web experiences can add value to your projects. Let's connect and talk about how I can contribute to the success of your project! Don't hesitate to contact me so that together we can create something amazing! 📱+5492281531457 📧 puntonetazul@gmail.com",
+};
+const worktimeline = [{
+        jobtitle: "Final project - Aprende Conmigo",
+        where: "Henry",
+        date: "2023",
+    },
+    {
+        jobtitle: "Individual project - Dogs",
+        where: "Henry",
+        date: "2023",
+    },
+    {
+        jobtitle: "Individual project - Drivers",
+        where: "Henry",
+        date: "2023",
+    },
+    {
+        jobtitle: "Technical support - .NET Informática",
+        where: "Owner",
+        date: "1995-2023",
+    },
+    {
+        jobtitle: "Insurance agent - Maria Laura Lobrundo",
+        where: "Insurance",
+        date: "2016-2023",
+    },
+];
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Axios",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 60,
+    },
+    {
+        name: "PostGresSQL",
+        value: 90,
+    },
+    {
+        name: "Express",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Design of web app graphic solutions to achieve a better impact on your audience.",
+    },
+    {
+        title: "Full Stack Developer",
+        description: "Front and Back of web applications, SPA, connection and data persistence, web APIS, JS | react | Axios | Express | Sequelize | PostGresSQL",
+    },
+
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "puntonetazul@gmail.com",
+    YOUR_FONE: "+54 (2281)53-1457",
+    description: "Let's connect and talk about how I can contribute to the success of your project! Don't hesitate to contact me so that together we can create something amazing!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_cwze3jl",
+    YOUR_TEMPLATE_ID: "template_hcekgyg",
+    YOUR_USER_ID: "NOJB7y0wM8LRLnFeY",
+};
+
+const socialprofils = {
+    github: "https://github.com/JorgeAlfredoLozano",
+    facebook: "",
+    linkedin: "https://www.linkedin.com/in/jorgealozano/",
+    twitter: "",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
